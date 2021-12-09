554c3ae05ac54b60b6e10676b6666bbea6f542bb
Lysergid <zzikk@mail.ru>
2021-12-09 14:17:22 +0100

    Basic table layout

--- re-wiser-web/src/main/frontend/src/App.tsx
+++ re-wiser-web/src/main/frontend/src/App.tsx
@@ -1,53 +1,12 @@
 import React from 'react';
 import './App.css';
-import {DataGrid, GridColDef} from '@mui/x-data-grid';
-
-
-const columns: GridColDef[] = [
-    {field: 'id', headerName: 'ID', width: 70},
-    {field: 'from', headerName: 'From', width: 230},
-    {field: 'to', headerName: 'To', width: 230},
-    {field: 'subject', headerName: 'Subject', width: 200,},
-    {field: 'body', headerName: 'Message', width: 90,},
-    {field: 'attachments', headerName: 'Attachments', width: 100}
-    /*{
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params: GridValueGetterParams) =>
-            `${params.getValue(params.id, 'to') || ''} ${
-                params.getValue(params.id, 'from') || ''
-            }`,
-    },*/
-];
-
-const rows = [
-    {id: 1, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-    {id: 2, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-    {id: 3, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-    {id: 4, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-    {id: 5, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-    {id: 6, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-    {id: 7, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-    {id: 8, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-    {id: 9, from: 'Snow', to: 'Jon', subject: 'subject', body: 'body', attachments: 'attachments'},
-];
-
+import CollapsibleTable from "./CollapsibleTable";
 
 function App() {
     return (
         <div className="App">
             <div style={{height: 400, width: '100%'}}>
-
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                />
+                <CollapsibleTable/>
             </div>
         </div>
     );
---
+++ re-wiser-web/src/main/frontend/src/CollapsibleTable.tsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+function createData(
+    id: number,
+    from: string,
+    to: string,
+    subject: string,
+    message: string,
+    date: string,
+    attachments: Array<Attachment>
+) {
+    return {
+        id,
+        from,
+        to,
+        subject,
+        message,
+        date,
+        attachments
+    };
+}
+
+function Row(props: { row: ReturnType<typeof createData> }) {
+    const {row} = props;
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <React.Fragment>
+            <TableRow sx={{'& > *': {borderBottom: 'unset'}}}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
+                    </IconButton>
+                </TableCell>
+                <TableCell align="right">{row.id}</TableCell>
+                <TableCell align="right">{row.from}</TableCell>
+                <TableCell align="right">{row.to}</TableCell>
+                <TableCell align="right">{row.subject}</TableCell>
+                <TableCell align="right">{row.date}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{margin: 1}}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Message
+                            </Typography>
+                            <Typography variant="body2" gutterBottom component="div">
+                                {row.message}
+                            </Typography>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Attachments
+                            </Typography>
+                            <Table size="small" aria-label="purchases">
+                                <TableBody>
+                                    {row.attachments.map((attachment) => (
+                                        <TableRow key={attachment.id}>
+                                            <TableCell component="th" scope="row">
+                                                {attachment.name}
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+}
+
+const attachments =
+    [
+        {
+            name: 'important file 1',
+            id: 11091700,
+        },
+        {
+            name: 'important file 1',
+            id: 23452,
+        },
+    ];
+
+type Attachment = {
+    id: number,
+    name: string
+
+};
+
+const rows = [
+    createData(1, 'from', 'to', 'subject', 'message', 'today', attachments),
+    createData(2, 'from', 'to', 'subject', 'message', 'today', attachments),
+    createData(3, 'from', 'to', 'subject', 'message', 'today', attachments),
+];
+
+export default function CollapsibleTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell/>
+                        <TableCell>Id</TableCell>
+                        <TableCell align="right">From</TableCell>
+                        <TableCell align="right">To</TableCell>
+                        <TableCell align="right">Subject</TableCell>
+                        <TableCell align="right">Date</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <Row key={row.id} row={row}/>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
