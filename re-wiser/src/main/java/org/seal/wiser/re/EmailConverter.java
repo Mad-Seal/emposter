a2c5042dc4e33d697371944e35200c7ffae92b7a
Lysergid <zzikk@mail.ru>
2022-01-20 22:09:57 +0100

    #1: Add license

--- re-wiser/src/main/java/org/seal/wiser/re/EmailConverter.java
+++ re-wiser/src/main/java/org/seal/wiser/re/EmailConverter.java
@@ -1,8 +1,29 @@
 package org.seal.wiser.re;
 
+/*-
+ * #%L
+ * re-wiser
+ * %%
+ * Copyright (C) 2022 authors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import jakarta.mail.*;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMultipart;
+
 import lombok.SneakyThrows;
 import org.seal.wiser.backend.Attachment;
 import org.seal.wiser.backend.Email;
